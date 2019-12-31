52211132625b910bd1645de1581ac033f70f20e3
Victor Saa <victorsaagbmediagroup@gmail.com>
2019-12-30 19:37:09 -0500

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// Log message
+console.log('%c###%c Made with ❤ using Javascript\n%c##%c  Developed & mantained by @vis97c\n%c#%c   Get in touch with me on Twitter or GitHub',
+  'background:#f1c40f;color:transparent;padding:3px 0', 'padding:3px 0',
+  'background:#2980b9;color:transparent;padding:3px 0', 'padding:3px 0',
+  'background:#e74c3c;color:transparent;padding:3px 0', 'padding:3px 0');
+
+window.$ = window.jQuery = require('jquery');
