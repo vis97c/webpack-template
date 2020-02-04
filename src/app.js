cf6a8a4a19c4a2a3afabcb129174b56105604cf0
Victor Saa <victorsaagbmediagroup@gmail.com>
2020-02-04 17:41:06 -0500

    prettier support + custom css variables
    there were some code revisions too

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
-// Log message
-console.log('%c###%c Made with ❤ using Javascript\n%c##%c  Developed & mantained by @vis97c\n%c#%c   Get in touch with me on Twitter or GitHub',
-  'background:#f1c40f;color:transparent;padding:3px 0', 'padding:3px 0',
-  'background:#2980b9;color:transparent;padding:3px 0', 'padding:3px 0',
-  'background:#e74c3c;color:transparent;padding:3px 0', 'padding:3px 0');
+/** @format */
+/*globals*/
+console.log(
+	"%c###%c Made with ❤ using Javascript\n%c##%c  Developed & mantained by @vis97c\n%c#%c   Get in touch with me on Twitter or GitHub",
+	"background:#f1c40f;color:transparent;padding:3px 0",
+	"padding:3px 0",
+	"background:#2980b9;color:transparent;padding:3px 0",
+	"padding:3px 0",
+	"background:#e74c3c;color:transparent;padding:3px 0",
+	"padding:3px 0"
+);
 
-window.$ = window.jQuery = require('jquery');
+window.$ = window.jQuery = require("jquery");
+import "_scss/main.scss";
--- webpack.config.js
+++ webpack.config.js
@@ -1,208 +1,211 @@
-require('dotenv').config({path: '.env'});
+/** @format */
+/*globals */
+require("dotenv").config({ path: ".env" });
 
-const CopyPlugin = require('copy-webpack-plugin');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-require('es6-promise').polyfill();
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+require("es6-promise").polyfill();
 
-const mode = process.env.NODE_ENV || 'development';
-const isProduction = mode === 'production';
+const mode = process.env.NODE_ENV || "development";
+const isProduction = mode === "production";
 //webpack defaults
 var config = {
-    entry: {
-        polyfill: '@babel/polyfill',
-        main: './src/app.js'
-    },
-    resolve: {
-        alias: {
-            '_src': path.resolve(__dirname, 'src'),
-            '_assets': path.resolve(__dirname, 'src/assets'),
-        },
-        extensions: ['.js'],
-    },
-    output: {
-        path: __dirname + '/public_html',
-        filename: 'js/[name].js',
-        chunkFilename: 'js/lazy/[name].js',
-        publicPath: '/',
-    },
-    plugins: [],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css$/i,
-                use: [
+	entry: {
+		polyfill: "@babel/polyfill",
+		main: "./src/app.js",
+	},
+	resolve: {
+		alias: {
+			_src: path.resolve(__dirname, "src"),
+			_assets: path.resolve(__dirname, "src/assets"),
+			_scss: path.resolve(__dirname, "src/scss"),
+		},
+		extensions: [".js"],
+	},
+	output: {
+		path: __dirname + "/public_html",
+		filename: "js/[name].js",
+		chunkFilename: "js/lazy/[name].js",
+		publicPath: "/",
+	},
+	plugins: [],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+			{
+				test: /\.css$/i,
+				use: [
 					/**
 					 * MiniCssExtractPlugin doesn't support HMR.
 					 * For developing, use 'style-loader' instead.
 					 * */
-                    isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
-                    'css-loader' + (!isProduction ? '?sourceMap' : ''),
-                ]
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    isProduction ? MiniCssExtractPlugin.loader : 'tyle-loader',
-                    // Translates CSS into CommonJS
-                    'css-loader' + (!isProduction ? '?sourceMap' : ''),
-                    // Compiles Sass to CSS
-                    'sass-loader' + (!isProduction ? '?sourceMap' : ''),
-                ],
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: 'images/[name].[ext]',
-                        }
-                    }
-                ],
-                
-            },
-            {
-                test: /\.(mov|mp4|webm)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: 'video/[name].[ext]',
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: 'fonts/[name].[ext]',
-                        }
-                    }
-                ],
-            },
-        ]
-    },
-    mode,
-    devtool: isProduction ? false : 'source-map',
+					isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+					"css-loader" + (!isProduction ? "?sourceMap" : ""),
+				],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// Creates `style` nodes from JS strings
+					isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+					// Translates CSS into CommonJS
+					"css-loader" + (!isProduction ? "?sourceMap" : ""),
+					// Compiles Sass to CSS
+					"sass-loader" + (!isProduction ? "?sourceMap" : ""),
+				],
+			},
+			{
+				test: /\.(png|jpg|gif|svg)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "images/[name].[ext]",
+						},
+					},
+				],
+			},
+			{
+				test: /\.(mov|mp4|webm)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "video/[name].[ext]",
+						},
+					},
+				],
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "fonts/[name].[ext]",
+						},
+					},
+				],
+			},
+		],
+	},
+	mode,
+	devtool: isProduction ? false : "source-map",
 };
 if (isProduction) {
-    //production only
-    const TerserPlugin = require('terser-webpack-plugin');
-    const HtmlBeautifyPlugin = require('html-beautify-webpack-plugin');
-    const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-    const PurgecssPlugin = require('purgecss-webpack-plugin')
-    const glob = require('glob');
-    const PATHS = {
-        src: path.join(__dirname, 'src')
-    }
-    config.plugins.push(
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].css',
-        }),
-        new PurgecssPlugin({
-            paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
-            whitelistPatterns: [/swal/]
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'src/index.original.html',
-            hash: true,
-            minify: {
-                removeComments: true,
-                removeEmptyElements: false,
-                minifyCSS: {
-                    format: 'beautify'
-                }
-            }
-        }),
-        new HtmlBeautifyPlugin({
-            config: {
-                html: {
-                    end_with_newline: false,
-                    indent_size: 2,
-                    indent_with_tabs: true,
-                    indent_inner_html: true,
-                    preserve_newlines: false,
-                }
-            }
-        }),
-        new CopyPlugin([
-            // { from: 'src/to_public/default', to: '', dot: true },
-            { from: 'src/to_public/production', to: '', dot: true },
-        ]),
-    );
-    module.exports = Object.assign({}, config, {
-        optimization: {
-        	splitChunks: {
-        		chunks: 'all',
-            },
-            minimize: true,
-            minimizer: [
-                new TerserPlugin({
-                    cache: true,
-                    parallel: true,
-                    terserOptions: {
-                        output: {
-                            comments: false,
-                        },
-                    },
-                    extractComments: false,
-                }),
-                new OptimizeCSSAssetsPlugin({
-                    cssProcessor: require('cssnano'),
-                    cssProcessorPluginOptions: {
-                        'preset': [
-                            'default', {
-                                'discardComments': {
-                                    'removeAll': true
-                                }
-                            }
-                        ]
-                    },
-                })
-            ],
-        },
-    });
+	//production only
+	const TerserPlugin = require("terser-webpack-plugin");
+	const HtmlBeautifyPlugin = require("html-beautify-webpack-plugin");
+	const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+	const PurgecssPlugin = require("purgecss-webpack-plugin");
+	const glob = require("glob");
+	const PATHS = {
+		src: path.join(__dirname, "src"),
+	};
+	config.plugins.push(
+		new MiniCssExtractPlugin({
+			filename: "css/[name].css",
+		}),
+		new PurgecssPlugin({
+			paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
+			whitelistPatterns: [/swal/],
+		}),
+		new HtmlWebpackPlugin({
+			filename: "index.html",
+			template: "src/index.original.html",
+			hash: true,
+			minify: {
+				removeComments: true,
+				removeEmptyElements: false,
+				minifyCSS: {
+					format: "beautify",
+				},
+			},
+		}),
+		new HtmlBeautifyPlugin({
+			config: {
+				html: {
+					end_with_newline: false,
+					indent_size: 2,
+					indent_with_tabs: true,
+					indent_inner_html: true,
+					preserve_newlines: false,
+				},
+			},
+		}),
+		new CopyPlugin([
+			// { from: 'src/to_public/default', to: '', dot: true },
+			{ from: "src/to_public/production", to: "", dot: true },
+		])
+	);
+	module.exports = Object.assign({}, config, {
+		optimization: {
+			splitChunks: {
+				chunks: "all",
+			},
+			minimize: true,
+			minimizer: [
+				new TerserPlugin({
+					cache: true,
+					parallel: true,
+					terserOptions: {
+						output: {
+							comments: false,
+						},
+					},
+					extractComments: false,
+				}),
+				new OptimizeCSSAssetsPlugin({
+					cssProcessor: require("cssnano"),
+					cssProcessorPluginOptions: {
+						preset: [
+							"default",
+							{
+								discardComments: {
+									removeAll: true,
+								},
+							},
+						],
+					},
+				}),
+			],
+		},
+	});
 } else {
-    //dev only
-    const WebpackNotifierPlugin = require('webpack-notifier');
-    config.plugins.push(
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'src/index.original.html',
-            hash: true,
-        }),
-        new CopyPlugin([
-            // { from: 'src/to_public/default', to: '', dot: true },
-            { from: 'src/to_public/dev', to: '', dot: true },
-        ]),
-        new WebpackNotifierPlugin({
-            title: 'Webpack Build',
-            contentImage: path.join(__dirname, 'src/assets/logo.png'),
-            alwaysNotify: true,
-        }),
-    );
-    module.exports = Object.assign({}, config, {
-        devServer: {
-            port: 3000,
-            hot: false,
-            inline: true,
-            contentBase: 'public_html',
-            historyApiFallback: true,
-            open: true,
-        },
-    });
+	//dev only
+	const WebpackNotifierPlugin = require("webpack-notifier");
+	config.plugins.push(
+		new HtmlWebpackPlugin({
+			filename: "index.html",
+			template: "src/index.original.html",
+			hash: true,
+		}),
+		new CopyPlugin([
+			// { from: 'src/to_public/default', to: '', dot: true },
+			{ from: "src/to_public/dev", to: "", dot: true },
+		]),
+		new WebpackNotifierPlugin({
+			title: "Webpack Build",
+			contentImage: path.join(__dirname, "src/assets/logo.png"),
+			alwaysNotify: true,
+		})
+	);
+	module.exports = Object.assign({}, config, {
+		devServer: {
+			port: 3000,
+			hot: false,
+			inline: true,
+			contentBase: "public_html",
+			historyApiFallback: true,
+			open: true,
+		},
+	});
 }
